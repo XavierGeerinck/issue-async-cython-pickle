1dc22d34ff90d6cb07f1825c551aa433dde0de54
Xavier Geerinck <xavier.geerinck@gmail.com>
2024-02-14 13:51:38 +0100

    add test case for detection of coroutine
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- my_module/tests/test_async_cython.py
+++ my_module/tests/test_async_cython.py
@@ -6,30 +6,24 @@ from my_module.demo_counter import DemoCounter
 
 DEMO_CONFIG = {"config": "test"}
 
+def test_is_coroutine():
+    a = DemoCounter(12)
+
+    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
+    assert a.get_counter_async().__class__.__name__.rsplit('.', 1)[-1] in ('coroutine', '_GeneratorWrapper')
+
 
 def test_cloudpickleray_coroutine():
-    """
-    """
     # Test Pickle
     a = DemoCounter(12)
-    
-    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
     ray._private.serialization.pickle_dumps(a.get_counter_async(), "ERROR PICKLING")
 
 
 def test_cloudpickle_coroutine():
-    # Test Pickle
     a = DemoCounter(12)
-    
-    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
     cloudpickle.dumps(a.get_counter_async())
     
 
 def test_pickle_coroutine():
-    """
-    """
-    # Test Pickle
     a = DemoCounter(12)
-    
-    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
     pickle.dumps(a.get_counter_async())
