5f693383e2a6b820affb7f0820424611aabb8b82
Xavier Geerinck <xavier.geerinck@gmail.com>
2024-02-14 13:46:37 +0100

    add test cases for cloudpickle, pickle and cloudpickle ray
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- my_module/tests/test_async_cython.py
+++ my_module/tests/test_async_cython.py
@@ -1,25 +1,35 @@
 import pickle
 import ray
+import cloudpickle
 
 from my_module.demo_counter import DemoCounter
 
 DEMO_CONFIG = {"config": "test"}
 
 
+def test_cloudpickleray_coroutine():
+    """
+    """
+    # Test Pickle
+    a = DemoCounter(12)
+    
+    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
+    ray._private.serialization.pickle_dumps(a.get_counter_async(), "ERROR PICKLING")
+
+
+def test_cloudpickle_coroutine():
+    # Test Pickle
+    a = DemoCounter(12)
+    
+    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
+    cloudpickle.dumps(a.get_counter_async())
+    
+
 def test_pickle_coroutine():
     """
     """
     # Test Pickle
     a = DemoCounter(12)
-    print(a.get_counter_async())
-    a_serialized = ray._private.serialization.pickle_dumps(a.get_counter_async(), "ERROR PICKLING")
-    a_deserialized = pickle.loads(a_serialized)
-
-
-    # Test Cloudpickle
-    a = DemoCounter(12)
-    print(a.get_counter_async())
-    a_serialized = ray.cloudpickle.dumps(a.get_counter_async())
-    a_deserialized = ray.cloudpickle.loads(a_serialized)
-
-    # assert a.counter == a_deserialized
+    
+    # a.get_counter_async() is a <_cython_3_0_8.coroutine object at 0x7f5b595e1f40> 
+    pickle.dumps(a.get_counter_async())
